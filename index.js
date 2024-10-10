c60e9b0b22f39bf40c138135782ceace92948ddc
kosal.sim <kosal.sim@ecoinsoft.com>
2024-10-10 18:01:10 +0700

    first init

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Replace with your actual bot token
+const BOT_TOKEN = "6546219051:AAHmWKloQQurtSw9LqOSuseKb3RSvwNqZeA";
+
+app.use(bodyParser.json());
+
+app.post("/webhook", (req, res) => {
+  const message = req.body.message;
+
+  if (message) {
+    const chatId = message.chat.id;
+    const responseText = "Webhook: You are connnected!";
+
+    // Send a reply to the user
+    axios
+      .post(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+        chat_id: chatId,
+        text: responseText,
+      })
+      .then(() => {
+        res.send("Message sent");
+      })
+      .catch((err) => {
+        console.error("Error sending message:", err);
+        res.send("Error sending message");
+      });
+  } else {
+    res.send("No message received");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Webhook server is running on port ${PORT}`);
+});
